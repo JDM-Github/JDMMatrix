0a692ab672c03d261ce6f956e4ed3f3cee65519e
JDMaster <numberonejdmaster@gmail.com>
2023-02-11 14:27:05 +0800

    Changes to be committed: 	modified:   .gitignore 	modified:   test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-# from kivy.uix.label import Label
-# from kivy.uix.image import Image
-
-# class Label2(Label): ...
-
-# class NewImage(Image):
-#     def __init__(self, **kwargs):
-#         super().__init__(**kwargs)
-#         self.source = "asset/mainBg0.png"
